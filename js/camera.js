a65a1a60604a23ef12f4dd46f0fc76542514959b
desertblade <desertblade@gmail.com>
2016-07-20 12:11:25 -0700

    Create camera.js

---
+++ js/camera.js
@@ -0,0 +1,17 @@
+$('.button').click(function(e){
+    var show = $(this).attr("data-show-id");
+    var hide = $(this).attr("data-hide-id");
+    
+    $(show).css(
+        {
+            'display': "block",
+            'position': "static"
+        }
+    );
+    $(hide).css(
+        {
+            'display': "none",
+            'position': "absolute"
+        }
+    );
+});
